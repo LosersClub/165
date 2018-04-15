f488771f6ebc3c72379dd7d96679b1a8fd9d8f43
Kyle Bartz <kyle@unleashurgeek.com>
2018-04-15 12:43:53 -0700

    Added error prints to the top of doalg

--- ProjectOne/doalg.c
+++ ProjectOne/doalg.c
@@ -6,6 +6,15 @@
 // Kyle Bartz (27024310)
 
 int doalg(int n, int k, int* out) {
+  if (n < 10 || n > 10000) {
+    printf("Error: n must be between 10 and 10000\n");
+    return -1;
+  }
+  if (k < 1 || k > n || k > 100) {
+    printf("Error: k must be between 1 and 100 and less than n\n");
+    return -1;
+  }
+
   tournamentSelect(n, k, out);
 
   // Converting our 0-based to 1-based indexing for COMPARE.c
