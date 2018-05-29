dcd93cc21108ef42e45ebb4361659981edbbde27
Kyle Bartz <kyle@unleashurgeek.com>
2018-05-29 15:29:06 -0700

    Refactor main for LZ

--- ProjectThree/compress.cpp
+++ ProjectThree/compress.cpp
@@ -8,46 +8,53 @@
 // TODO: The filename should be a command-line arg, as well as the parameters
 int main() {
   std::string str = "ABBABABBABBABAAAABABABA";
-  //std::string str = "ABA";
-  //Window window = Window(5, 2, str.c_str(), 3);
-  //window.shift(2);
-  //window.print();
-  //std::cout << std::endl;
   int strIndex = 6;
   Window window = Window(8, 5, str.substr(0, 6).c_str(), 6);
   SuffixArray sa(&window);
 
+  std::pair<int, std::vector<char>> triple = { 1, {window.getFromDict(0)} };
+  bool lastTriple = true;
   
   std::cout << "String: " << str << std::endl;
-  std::cout << "(0,1," << window.getFromDict(0) << ")" << std::endl; // First
-  while (strIndex < str.length()) {
-    std::pair<int, int> result = sa.getMatch();
-    if (result.first < 2) {
-      std::cout << "(0,1," << window.getFromLab(0) << ")" << std::endl;
-      window.add(str.at(strIndex++));
-    } else {
-      std::cout << "(" << result.first << "," << result.second << ")" << std::endl;
-      int end = strIndex + result.first > str.length() ? str.length() - strIndex : result.first;
-      for (int i = 0; i < end; i++) {
-        window.add(str.at(strIndex + i));
-      }
-      if (strIndex + result.first > str.length()) {
-        window.shift((strIndex + result.first) - str.length());
-      }
-      strIndex += result.first;
-    }
-    sa.rebuild();
-  }
 
   while (window.getLabSize() > 0) {
     std::pair<int, int> result = sa.getMatch();
     if (result.first < 2) {
-      std::cout << "(0,1," << window.getFromLab(0) << ")" << std::endl;
-      window.shift(1);
-    }
-    else {
+      if (lastTriple) {
+        triple.first += 1;
+        triple.second.push_back(window.getFromLab(0));
+      }
+      else {
+        triple = { 1, {window.getFromLab(0)} };
+        lastTriple = true;
+      }
+      if (strIndex < str.length()) {
+        window.add(str.at(strIndex++));
+      } else {
+        window.shift(1);
+      }
+    } else {
+      if (lastTriple) {
+        std::cout << "(0," << triple.first << ",";
+        for (char& c : triple.second) {
+          std::cout << c;
+        }
+        std::cout << ")" << std::endl;
+        lastTriple = false;
+      }
       std::cout << "(" << result.first << "," << result.second << ")" << std::endl;
-      window.shift(result.first);
+      if (strIndex < str.length()) {
+        int end = strIndex + result.first > str.length() ? str.length() - strIndex : result.first;
+        for (int i = 0; i < end; i++) {
+          window.add(str.at(strIndex + i));
+        }
+        if (strIndex + result.first > str.length()) {
+          window.shift((strIndex + result.first) - str.length());
+        }
+        strIndex += result.first;
+      } else {
+        window.shift(result.first);
+      }
     }
     sa.rebuild();
   }
