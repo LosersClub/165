5c41fe89de0994074ec59713a4b6ccb0b98ac30b
Anastasia Miles <anastasiarosemiles@gmail.com>
2018-06-03 11:22:32 -0700

    Modify compress to work with files

--- ProjectThree/compress.cpp
+++ ProjectThree/compress.cpp
@@ -1,21 +1,99 @@
 
 #include "window.h"
+#include "file.h"
 #include "suffix_array.h"
+#include "bit_stream.h"
 #include <iostream>
 #include <string>
 #include <tuple>
+#include <cmath>
+#include <bitset>
+
+bool parseArguments(int argc, char**argv, int* N, int* L, int* S, std::string* path) {
+  if (argc > 5) {
+    std::cerr << "Too many arguments." << std::endl;
+    return false;
+  }
+  if (argc < 2) {
+    std::cerr << "One required argument: path to file to be compressed." << std::endl;
+    return false;
+  }
+  for (int i = 1; i < argc; i++) {
+    if (argv[i][0] != '-') {
+      if (*path != "") {
+        std::cerr << "Multiple candidates for file to be compressed." << std::endl;
+        return false;
+      }
+      *path = std::string(argv[i]);
+    }
+    else {
+      int number = std::stoi(std::string(argv[i]).substr(3));
+      if (argv[i][1] == 'N') {
+        if (number < 9 || number > 14) {
+          std::cerr << "N argument out of range." << std::endl;
+          return false;
+        }
+        *N = number;
+      }
+      else if (argv[i][1] == 'L') {
+        if (number < 3 || number > 4) {
+          std::cerr << "L argument out of range." << std::endl;
+          return false;
+        }
+        *L = number;
+      }
+      else if (argv[i][1] == 'S') {
+        if (number < 1 || number > 5) {
+          std::cerr << "S argument out of range." << std::endl;
+          return false;
+        }
+        *S = number;
+      }
+      else {
+        std::cerr << "Unrecognized argument: " << argv[i] << std::endl;
+      }
+    }
+  }
+
+}
 
 // TODO: The filename should be a command-line arg, as well as the parameters
-int main() {
-  std::string str = "AABDBBABABBABCCCABBBABAAAACCABBABACBA";
-  int strIndex = 6;
-  Window window = Window(16, 5, str.substr(0, 6).c_str(), 6);
+int main(int argc, char** argv) {
+  // default values
+  int N = 11;
+  int L = 4;
+  int S = 3;
+  std::string path = "";
+  // update args
+  if (!parseArguments(argc, argv, &N, &L, &S, &path)) {
+    return 0;
+  }
+  if (path == "") {
+    std::cout << "No file path set." << std::endl;
+    return 0;
+  }
+
+  int windowSize = pow(2, N);
+  int labSize = pow(2, L);
+  int maxTripleLength = pow(2, S) - 1;
+
+  BitStreamWriter writer = BitStreamWriter(N, L, S);
+  std::cout << writer.writeHeader();
+
+  File file = File(path);
+  std::string init_string;
+  int index = 0;
+  bool y = file.hasNextChar();
+  while (index < labSize + 1 && file.hasNextChar()) {
+    init_string += file.readChar();
+    index++;
+  }
+
+  Window window = Window(windowSize, labSize, init_string.c_str(), index);
   SuffixArray sa(&window);
 
   std::pair<int, std::vector<char>> triple = { 1, {window.getFromDict(0)} };
   bool lastTriple = true;
-  
-  std::cout << "String: " << str << std::endl;
 
   while (window.getLabSize() > 0) {
     std::pair<int, int> result = sa.getMatchBinarySearch();
@@ -28,37 +106,36 @@ int main() {
         triple = { 1, {window.getFromLab(0)} };
         lastTriple = true;
       }
-      if (strIndex < str.length()) {
-        window.add(str.at(strIndex++));
+      if (file.hasNextChar()) {
+        index++;
+        window.add(file.readChar());
       } else {
         window.shift(1);
       }
     } else {
       if (lastTriple) {
-        std::cout << "(0," << triple.first << ",";
-        for (char& c : triple.second) {
-          std::cout << c;
-        }
-        std::cout << ")" << std::endl;
+        writer.writeTriple(triple.first, triple.second);
         lastTriple = false;
       }
-      std::cout << "(" << result.first << "," << result.second << ")" << std::endl;
-      if (strIndex < str.length()) {
-        int end = strIndex + result.first > str.length() ? str.length() - strIndex : result.first;
-        for (int i = 0; i < end; i++) {
-          window.add(str.at(strIndex + i));
+      writer.writeDouble(result.first, result.second);
+      if (file.hasNextChar()) {
+        int i = 0;
+        for (i = 0; i < result.first && file.hasNextChar(); i++) {
+          window.add(file.readChar());
         }
-        if (strIndex + result.first > str.length()) {
-          window.shift((strIndex + result.first) - str.length());
+        if (!file.hasNextChar()) {
+          window.shift(result.first - i);
         }
-        strIndex += result.first;
       } else {
         window.shift(result.first);
       }
     }
     sa.rebuild();
-    //window.print();
-    //sa.print();
   }
-  system("PAUSE");
+  if (lastTriple) {
+    writer.writeTriple(triple.first, triple.second);
+  }
+  writer.writeEOF();
+  file.close();
+  return 1;
 }
