060d181ed239141ae106a2fc42e62c6ea932317a
Anastasia Miles <anastasiarosemiles@gmail.com>
2018-06-03 11:14:02 -0700

    Updated bit_stream for class-specified encoding and added getN function

--- ProjectThree/bit_stream.h
+++ ProjectThree/bit_stream.h
@@ -41,6 +41,9 @@ class BitStreamReader : public BitStream {
 
     std::pair<int, int> getDouble();
     std::pair<int, std::vector<char>> getTriple();
+
+    int getN();
+
   private:
     Mask *nMask, *lMask, *sMask;
     unsigned short overflow = 0x00;
